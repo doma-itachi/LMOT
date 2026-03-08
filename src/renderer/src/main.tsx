947e3186ff57664af385229ec999e30e98fee947
itachi <94786908+doma-itachi@users.noreply.github.com>
2026-03-08 12:27:29 +0900

    Add some shadcn components

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -3,9 +3,12 @@ import './assets/main.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { TooltipProvider } from './components/ui/tooltip'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TooltipProvider>
+      <App />
+    </TooltipProvider>
   </StrictMode>
 )
